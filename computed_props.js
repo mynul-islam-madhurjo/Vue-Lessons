907a900ff24c1b622305113d77f66ff8896042a8
mynul.islam <iammynul.bracu@gmail.com>
2022-12-28 15:13:15 +0600

    First Commit

---
+++ computed_props.js
@@ -0,0 +1,35 @@
+
+// This is a computed example with form
+/* var vm = new Vue({
+    el: '#computed_props',
+    data: {
+        firstname: "",
+        lastname: ""
+    },
+    computed: {
+        // With computed it gets called by itself. It is a object like method but when a method gets
+        // called everytime where computed is static it stays fixed 
+        getfullname() {
+            return this.firstname + " " + this.lastname;
+        }
+    }
+})
+ */
+
+//Method vs Computed Difference
+/* var vm1 = new Vue({
+    el: '#computed-methods',
+    data: {
+        name:'Hello'
+    },
+    computed: {
+        getComputedData(){
+            return Math.random();
+        }
+    },
+    methods: {
+        getMethodData(){
+            return Math.random();
+        }
+    }
+}) */
