c14ef9d238315f602a9a7be62d6f019950588984
mynul.islam <iammynul.bracu@gmail.com>
2022-12-29 15:48:53 +0600

    class binding

--- binding.js
+++ binding.js
@@ -1,9 +1,21 @@
 /* Binding Helps to manipulate and assign values to html attributes
-also can chage the style and classes using v-bind
+also can chage the style and classes using v-bind/:
 */
+
+//Binding links to anchor tag
 var a = new Vue({
     el:'#databind',
     data: {
+        name: 'mynul',
         hreflink : "http://www.google.com"
+    }
+});
+
+//binding classes
+var b = new Vue({
+    el: '#classbind',
+    data: {
+        title: 'This is class binding',
+        isactive: true
     }
 });
